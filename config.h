15e81a94d50fae3f022562a97570aa1ccc51c8a4
WouterSpekkink <wahsp@tutanota.com>
2021-03-05 17:05:41 +0100

    Changed default colours and added password option based on Luke Smith's example.

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;   /* -F option; if 0; dmenu does't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -10,7 +11,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#563C5C" },
+	[SchemeSelHighlight] = { "#f1fa8c", "#bd93f9"},
+	[SchemeNormHighlight] = { "#f1fa8c", "#222222"},
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
